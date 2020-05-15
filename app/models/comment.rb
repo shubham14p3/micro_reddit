0a85808645dfc9e3b6606b82b94540fc5ed4d634
shubham14p3 <shubham14p3@gmail.com>
2020-05-16 01:49:11 +0530

    Added Commenting Section

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+	belongs_to :post, required: false
+	belongs_to :user, required: false
+	validates :body, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_165704) do
+ActiveRecord::Schema.define(version: 2020_05_15_194045) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
@@ -29,5 +39,7 @@ ActiveRecord::Schema.define(version: 2020_05_15_165704) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
