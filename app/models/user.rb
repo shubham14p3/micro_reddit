bd46fde5afdbfb59e68a61a406fd336bfe1fcdb8
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-05-18 10:05:41 -0500

    Fixed Rubocop offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-  has_many :comments  
+  has_many :comments
   validates :name, presence: true, length: { minimum: 7 }
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
