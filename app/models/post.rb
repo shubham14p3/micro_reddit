5b8c9619caa07d23f7d8373956ea5efa7e5192ad
shubham14p3 <shubham14p3@gmail.com>
2020-05-18 19:46:04 +0530

    Fixed Comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, required: false
-
   validates :user, presence: true
   validates :title, presence: true
   validates :body, presence: true
-
   validates :title, length: { minimum: 7 }
 end
